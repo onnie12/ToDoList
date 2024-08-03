82d9680c3b6cd54fb847758764d93b4e0ea97b3c
onnie12 <152686116+onnie12@users.noreply.github.com>
2024-08-03 16:26:17 +0300

    final commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function closePopup(){
 const textBox = document.getElementById("textbox");
 const taskCreate = document.getElementById("taskCreate");
 
+let i = 0;
 
 function createTask(){
     const input = document.getElementById("textbox").value;
@@ -18,4 +19,23 @@ function createTask(){
     const li = document.createElement("li");
     li.innerText = input
     myul.appendChild(li);
+    delButton = document.createElement("button");
+    li.appendChild(delButton)
+    delButton.className = "delbutton";
+    delButton.onclick = function () {
+    document.getElementById("myul").removeChild(li)
+    document.getElementsByClassName("delbutton").innerText = "X";
+    expandDiv()
+    };
+    
 }
+function delButton(id){
+    const li = document.getElementById(id).
+    li.remove();
+    
+}
+function expandDiv(){
+    const expandDiv = document.getElementById("box");
+    expandDiv.classList.add("box");
+}
+
